1af80ca75721b5d66111dbb0d640fe4103cbf0ec
shailesh_gabu_11 <116952795+Shailesh051211@users.noreply.github.com>
2026-03-27 14:39:51 +0530

    Refactor party window text handling in plugin

--- z_hold_party_text_z.py
+++ z_hold_party_text_z.py
@@ -6,61 +6,35 @@
     
     Function:
         Holds text message of party window's text field.
-    
-    Logic: 
-        store text whenever party window close.
-        Then whenever it's open, edit text field with stored text.
-    
-    Query:    
-        Why is there 'z' at first and last in file name?
-            first 'z' reason >
-              because want to compile this file at last.  
-              so this plugin can be compatible with other party window plugins.
-            last 'z' reason >
-              Nothing... for decoration purpose.
 """
 
 from __future__ import annotations 
-from typing import TYPE_CHECKING, cast
+from typing import cast
 
-# Ballistica API.
 import bauiv1 as bui
-
-# Ballistica Libraries.
 from bauiv1lib.party import PartyWindow
-
-if TYPE_CHECKING:
-    pass
     
-# ba_meta export plugin
+# ba_meta export babase.Plugin
 class plg(bui.Plugin):
     """ Our plugin type for the game """
     
     # The party window text field's text; that to be hold.
     text: str = ""
-    
-    # we gonna use decorators cause we need to make this mod compatible with others.
-    
     def new_init(func: function) -> function:
-    
-        def wrapper(*args, **kwargs) -> None:
-            # original code.
-            func(*args, **kwargs)
-            # Editing...
+        def wrapper(*args, **kwargs):
+            func(*args, **kwargs) # original code
             bui.textwidget(edit=args[0]._text_field, text=plg.text)
         
         return wrapper
-    # wrapping new code.
+        
     PartyWindow.__init__ = new_init(PartyWindow.__init__)    
 
-    def new_close(func: function) -> function:
-
+    def new_close(func):
         def wrapper(*args, **kwargs) -> None:
-            # storing...
+            
             plg.text = cast(str, bui.textwidget(query=args[0]._text_field)).strip()
-            # original code.
-            func(*args, **kwargs)
+            func(*args, **kwargs) # original code
             
         return wrapper
-    # wrapping new code.   
+        
     PartyWindow.close = new_close(PartyWindow.close)
